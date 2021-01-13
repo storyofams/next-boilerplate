fd56def3661038852dd4f28ae09c71373bc5c658
Maren Osnabrug <maren@storyofams.com>
2021-01-13 17:56:33 +0100

    refactor: 🔍  add storysort

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,3 +2,12 @@ import ProviderDecorator from "./ProviderDecorator";
 import "../public/static/fonts/stylesheet.css";
 
 export const decorators = [ProviderDecorator]
+
+export const parameters = {
+  options: {
+    storySort: {
+        method: 'alphabetical',
+        order: ['components'],
+    },
+  },
+};
