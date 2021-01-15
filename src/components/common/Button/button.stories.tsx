d87b4cac5f596b177588ad1b7916c62839a8cccd
MitchelvanBever <mitchel@storyofams.com>
2021-01-15 09:07:19 +0100

    docs: 📝  added default args for button variants

--- src/components/common/Button/button.stories.tsx
+++ src/components/common/Button/button.stories.tsx
@@ -14,19 +14,36 @@ export const Primary = (args) => (
   </Button>
 );
 
+Primary.args = {
+  variant: 'primary',
+};
+
 export const Secondary = (args) => (
   <Button {...args} variant="secondary">
     Lorem Ipsum
   </Button>
 );
+
+Secondary.args = {
+  variant: 'secondary',
+};
+
 export const Underline = (args) => (
   <Button {...args} variant="underline">
     Lorem Ipsum
   </Button>
 );
 
+Underline.args = {
+  variant: 'underline',
+};
+
 export const Link = (args) => (
   <Button {...args} href="/" variant="primary">
     Lorem Ipsum
   </Button>
 );
+
+Link.args = {
+  variant: 'link',
+};
