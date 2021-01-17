5c22a89716e6dd5a4afd9afafe936b831706cba7
Aidana <aidana@storyofams.com>
2021-01-17 23:56:51 +0100

    refactor(custom): 🔍  components
    status message rebassed

---
+++ src/components/common/StatusMessage/StatusMessage.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from 'react';
+import Alert from '@reach/alert';
+import { Text, BoxProps, TextProps } from 'rebass/styled-components';
+
+type status = 'default' | 'success' | 'warning' | 'error';
+
+interface StatusMessageProps extends BoxProps, TextProps {
+  status: status | Boolean;
+  type?: 'polite' | 'assertive';
+}
+// this component alerts screen readers when its rendered.
+// should be used together with a formfield + formik
+
+export const StatusMessage: FC<StatusMessageProps> = ({
+  status,
+  type,
+  children,
+  ...props
+}) => {
+  return (
+    <Text
+      color={status !== 'default' && status ? `${status}500` : `grey900`}
+      fontSize={2}
+      fontWeight="medium"
+      mt="1/4"
+      {...props}
+    >
+      <Alert type={type}>{children}</Alert>
+    </Text>
+  );
+};
+
+export default StatusMessage;
