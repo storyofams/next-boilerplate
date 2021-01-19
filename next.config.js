56bc89da11b9f8fd158ac9da17500753a074f1e8
Maren Osnabrug <maren@storyofams.com>
2021-01-19 10:54:23 +0100

    fix: remove automagically added env keys

--- next.config.js
+++ next.config.js
@@ -90,28 +90,6 @@ module.exports = withSourceMaps({
 
     return config;
   },
-  target: 'serverless',
-  transformManifest: (manifest) => ['/'].concat(manifest),
-  workboxOpts: {
-    swDest: 'static/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'https-calls',
-          networkTimeoutSeconds: 15,
-          expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-    ],
-  },
   basePath,
   async redirects() {
     return [
