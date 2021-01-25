8478f22d6d1a3fa4b42c08a7a9e1c426b02f79ef
Doeke Leeuwis <doeke@storyofams.com>
2021-01-25 09:45:25 +0100

    refactor: 🔍  small tweaks to default page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,36 @@
 import React from 'react';
 import { NextSeo } from 'next-seo';
-import { Flex, Text, Heading } from 'rebass/styled-components';
+import { Flex, Text, Heading, Box } from 'rebass/styled-components';
+
+const cover = { top: 0, bottom: 0, left: 0, right: 0 };
 
 const Home = () => (
-  <div>
+  <>
     <NextSeo title="Home" description="What will your Story be?" />
     <Flex
       bg="black"
-      p={1}
-      width="100%"
-      height="100vh"
       flexWrap="wrap"
       alignContent="start"
       justifyContent="space-evenly"
       overflow="hidden"
       sx={{
+        position: 'absolute',
+        ...cover,
         '&:after': {
           content: "''",
           position: 'fixed',
-          top: 0,
-          bottom: 0,
-          left: 0,
-          right: 0,
+          ...cover,
           background:
             'radial-gradient(circle at center bottom, rgba(0,0,0,0.5), rgba(0,0,0,0.8), rgba(0,0,0,0.9), rgba(0,0,0,1))',
         },
       }}
     >
-      {[...Array(5000)].map(() => (
+      {[...Array(5000)].map((_, i) => (
         <Text
+          key={i}
           color="grey800"
           fontWeight="bold"
-          fontSize="7"
+          fontSize={7}
           lineHeight="normal"
           width="32px"
           textAlign="center"
@@ -40,16 +39,17 @@ const Home = () => (
         </Text>
       ))}
     </Flex>
-    <Heading
-      fontSize="9"
-      color="white"
-      textAlign="center"
-      width="100%"
-      sx={{ position: 'absolute', top: '50vh', transform: 'translateY(-50%)' }}
-    >
-      What will your Story be?
-    </Heading>
-  </div>
+    <Box pt="50vh" sx={{ position: 'relative' }}>
+      <Heading
+        fontSize={9}
+        color="white"
+        textAlign="center"
+        sx={{ transform: 'translateY(-50%)' }}
+      >
+        What will your Story be?
+      </Heading>
+    </Box>
+  </>
 );
 
 export default Home;
